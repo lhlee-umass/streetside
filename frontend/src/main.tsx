b9327d3798b43627d780731dab63b800357612ad
David Zhan <zhandavidz@gmail.com>
2025-03-11 02:47:12 -0400

    setup: Reorganize pages around React Router
    - Moved all page components to `pages/` directory
    - Converted `App.tsx` to a layout component
    - Added `404NotFound` page for invalid routes
    - Updated routing in `main.tsx` to use `BrowserRouter`
    
    Works towards #86

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+import Home from './pages/Home'
+import NotFoundPage from './pages/404NotFound'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
 )
---
+++ frontend/src/pages/404NotFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div className="text-center mt-12">
+      <h1 className="text-4xl font-bold text-red-400">404</h1>
+      <p className="text-lg">Page Not Found</p>
+    </div>
+  )
+}
