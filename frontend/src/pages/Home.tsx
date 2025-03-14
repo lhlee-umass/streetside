9c9c526a3501ea06c55079ed213cd83ffba3f02d
Laurie Lee <102129349+lauriehlee@users.noreply.github.com>
2025-03-14 09:22:28 -0400

    layout: set up grid layout for multiple listings
    - Implement a dynamic container that adjusts based on screen size
    - Use CSS Grid to create layout
    - Layout should include reuse of reusable listing card component
    - Appropriate / agreed upon header title for the layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import ListingCard from '../components/listing-card/ListingCard'
 
-export default function Home() {
-  const [count, setCount] = useState(0)
-
+const Home = () => {
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="text-blue-300">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1 className="text-3xl font-bold text-center my-8">
+        Streetside Marketplace
+      </h1>
+
+      <div className="container mx-auto px-4">
+        {/* Grid layout for listings */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          <ListingCard />
+          <ListingCard />
+          <ListingCard />
+          <ListingCard />
+          <ListingCard />
+          <ListingCard />
+        </div>
       </div>
     </>
   )
 }
+
+export default Home
