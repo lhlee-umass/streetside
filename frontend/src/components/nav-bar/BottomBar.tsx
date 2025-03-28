7cb7de68e8db107d6997e0f3f9ce8e03bbb1864b
Landrick Diaz <diazlandrick@gmail.com>
2025-03-28 14:40:31 -0400

    Refactor BottomBar component to remove unused hooks and simplify icon imports

--- frontend/src/components/nav-bar/BottomBar.tsx
+++ frontend/src/components/nav-bar/BottomBar.tsx
@@ -1,63 +1,39 @@
-import React from 'react'
 import { Link } from 'react-router'
-import { useLocation } from 'react-router'
-import {
-  FaFilter as FilterIcon,
-  FaShoppingCart as CartIcon,
-  FaMapMarkerAlt as MapIcon,
-  FaUsers as CommunityIcon,
-} from 'react-icons/fa'
-import { useCart } from '../../hooks/useCart'
+import { FiFilter, FiShoppingCart, FiMap, FiUsers } from 'react-icons/fi'
 
-const BottomBar: React.FC = () => {
-  const location = useLocation()
-  const { cartCount } = useCart()
-
-  const isActive = (path: string) => {
-    return location.pathname === path
-  }
-
+export default function BottomBar() {
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-green-700 text-white p-3 shadow-lg z-10">
       <div className="container mx-auto flex justify-around items-center">
         <Link
           to="/filter"
-          className={`flex flex-col items-center ${isActive('/filter') ? 'text-green-300' : 'hover:text-green-200'}`}
+          className="flex flex-col items-center hover:text-green-200"
         >
-          <FilterIcon className="w-6 h-6" />
+          <FiFilter className="w-6 h-6" />
           <span className="text-xs mt-1">Filter</span>
         </Link>
         <Link
           to="/cart"
-          className={`flex flex-col items-center relative ${isActive('/cart') ? 'text-green-300' : 'hover:text-green-200'}`}
+          className="flex flex-col items-center hover:text-green-200"
         >
-          <div className="relative">
-            <CartIcon className="w-6 h-6" />
-            {cartCount > 0 && (
-              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-                {cartCount}
-              </span>
-            )}
-          </div>
+          <FiShoppingCart className="w-6 h-6" />
           <span className="text-xs mt-1">Cart</span>
         </Link>
         <Link
           to="/map"
-          className={`flex flex-col items-center ${isActive('/map') ? 'text-green-300' : 'hover:text-green-200'}`}
+          className="flex flex-col items-center hover:text-green-200"
         >
-          <MapIcon className="w-6 h-6" />
+          <FiMap className="w-6 h-6" />
           <span className="text-xs mt-1">Local</span>
         </Link>
         <Link
           to="/community"
-          className={`flex flex-col items-center ${isActive('/community') ? 'text-green-300' : 'hover:text-green-200'}`}
+          className="flex flex-col items-center hover:text-green-200"
         >
-          <CommunityIcon className="w-6 h-6" />
+          <FiUsers className="w-6 h-6" />
           <span className="text-xs mt-1">Community</span>
         </Link>
       </div>
     </nav>
   )
 }
-
-export default BottomBar
