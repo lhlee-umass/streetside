528af903ecab9db510db7875510ce578d238653e
Landrick Diaz <diazlandrick@gmail.com>
2025-03-30 20:19:17 -0400

    Update package dependencies and enhance App styling; remove unused CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import BottomBar from './components/nav-bar/BottomBar'
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="min-h-screen flex flex-col m-0 p-0 text-[#213547] bg-white dark:text-white dark:bg-black">
       <TopBar />
       <main className="flex-grow pt-20 pb-16 px-4">
         <Outlet />
