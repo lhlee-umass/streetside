20f8fce42a16235c4eb90ac62ed5062043e05e3b
Landrick Diaz <diazlandrick@gmail.com>
2025-04-04 18:56:42 -0400

    Pass id prop to ListingCard in Home component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -200,6 +200,7 @@ const Home = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
           {filteredListings.map((listing, index) => (
             <ListingCard
+              id={listing.id}
               key={index}
               image={listing.image}
               title={listing.title}
